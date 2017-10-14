ab1d4f067687213602bcfc79d1fe8c2cc2027719
Ameir Abdeldayem <ameirh@gmail.com>
2017-10-14 17:18:06 -0400

    Simplify the run.

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ AWS_DEFAULT_REGION=${AWS_REGION:-us-east-1}
 : ${ECS_CLUSTER_NAME:=default}
 : ${RESOURCE_CHECK_INTERVAL:=60}
 
-SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
-"$SCRIPT_DIR/ecspander.py" --cluster $ECS_CLUSTER_NAME
+./$APP_NAME.py --cluster $ECS_CLUSTER_NAME
